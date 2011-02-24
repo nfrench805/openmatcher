034163f173878a9e92e179dcedc362a87fd669b5
pascaldergane <pascaldergane@gmail.com>
2011-02-24 12:28:05 +0000

    update DFT algorithm and add new logo

--- matcher/src/main/java/com/quantum/algorithms/fourier/DFTMatcher.java
+++ matcher/src/main/java/com/quantum/algorithms/fourier/DFTMatcher.java
@@ -1,21 +1,23 @@
 package com.quantum.algorithms.fourier;
 
 import java.awt.geom.Point2D;
-import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
 
 import org.apache.commons.math.complex.Complex;
 
 public class DFTMatcher extends GenericMatcher implements IMatcher {
+
+	private Map<Integer, Complex> W1 = null;
+	private Map<Integer, Complex> W2 = null;
 
 	public double match(double[][] reference, double[][] candidate) {
 		int N1 = reference.length;
 		int N2 = reference[0].length;
 
 		logger.info("width=" + N1 + " height=" + N2);
-		//initialize(N1, N2);
+		W1 = initialize(N1);
+		W2 = initialize(N2);
 		// we suppose images with same size
 		logger.info("computing DFT of reference...");
 		Complex[][] F = get2D_DFT(reference);
@@ -30,7 +32,30 @@ public class DFTMatcher extends GenericMatcher implements IMatcher {
 		Complex score = POC[(int) peak.getX()][(int) peak.getY()];
 		return score.getReal();
 	}
-	
+
+	/**
+	 * initialize W
+	 * 
+	 * @param N1
+	 * @param N2
+	 */
+	public Map<Integer, Complex> initialize(final int N) {
+		logger.info("Initializing W...");
+		Map<Integer, Complex> W = new HashMap<Integer, Complex>();
+
+		for (int k = 0; k <= N / 2; k++) {
+			for (int n = 0; n <= N / 2; n++) {
+				double omega = Math.PI * 2 * k * n / N;
+				W.put(Integer.valueOf(k * n), new Complex(Math.cos(omega),
+						-Math.sin(omega)));
+				W.put(Integer.valueOf(-k * n), new Complex(Math.cos(omega),
+						Math.sin(omega)));
+			}
+		}
+
+		return W;
+	}
+
 	/**
 	 * Compute DFT element(k1,k2) of f(k1,k2)
 	 * 
@@ -42,17 +67,36 @@ public class DFTMatcher extends GenericMatcher implements IMatcher {
 	public Complex getDFT(final double[][] f, final int k1, final int k2) {
 		int N1 = f.length;
 		int N2 = f[0].length;
-		Complex F = new Complex(0, 0);
+		Complex F = new Complex(f[N1 / 2][N2 / 2], 0);
+		Complex W1 = this.W1.get(Integer.valueOf(-k1 * N1 / 2)).conjugate();
+		Complex W2 = this.W2.get(Integer.valueOf(-k2 * N2 / 2)).conjugate();
+		F = F.add(W1.multiply(W2).multiply(f[0][0]));
 
-		for (int n1 = -N1 / 2; n1 < N1 / 2; n1++) {
-			for (int n2 = -N2 / 2; n2 < N2 / 2; n2++) {
-				double theta1 = 2 * Math.PI * k1 * n1 / N1;
-				double theta2 = 2 * Math.PI * k2 * n2 / N2;
-				Complex W1 = new Complex(Math.cos(theta1), -Math.sin(theta1));
-				Complex W2 = new Complex(Math.cos(theta2), -Math.sin(theta2));
+		for (int n1 = 0; n1 < N1 / 2; n1++) {
+			for (int n2 = 0; n2 < N2 / 2; n2++) {
 
-				F = F.add(W1.multiply(W2).multiply(f[n1 + N1 / 2][n2 + N2 / 2]));
+				// double theta1 = 2 * Math.PI * k1 * n1 / N1;
+				// double theta2 = 2 * Math.PI * k2 * n2 / N2;
+				// Complex W1 = new Complex(Math.cos(theta1),
+				// -Math.sin(theta1));
+				// Complex W2 = new Complex(Math.cos(theta2),
+				// -Math.sin(theta2));
+				W1 = this.W1.get(Integer.valueOf(k1 * n1)).conjugate();
+				W2 = this.W2.get(Integer.valueOf(k2 * n2)).conjugate();
 
+				// n1>0 && n2>0
+				Complex a = W1.multiply(W2).multiply(
+						f[n1 + N1 / 2][n2 + N2 / 2]);
+				// n1>0 && n2<0
+				Complex b = W1.multiply(W2.conjugate()).multiply(
+						f[n1 + N1 / 2][-n2 + N2 / 2]);
+				// n1<0 && n2>0
+				Complex c = W1.conjugate().multiply(W2)
+						.multiply(f[-n1 + N1 / 2][n2 + N2 / 2]);
+				// n1<0 && n2<0
+				Complex d = W1.multiply(W2).conjugate()
+						.multiply(f[-n1 + N1 / 2][-n2 + N2 / 2]);
+				F = (n1 == 0 && n2 == 0) ? F : F.add(a).add(b).add(c).add(d);
 			}
 		}
 
@@ -92,21 +136,36 @@ public class DFTMatcher extends GenericMatcher implements IMatcher {
 	public Complex getIDFT(final Complex[][] R, final int n1, final int n2) {
 		int N1 = R.length;
 		int N2 = R[0].length;
-		Complex r = new Complex(0, 0);
+		Complex r = R[N1 / 2][N2 / 2];
+		Complex W1 = this.W1.get(Integer.valueOf(n1 * N1 / 2));
+		Complex W2 = this.W2.get(Integer.valueOf(n2 * N2 / 2));
+		r = r.add(W1.multiply(W2).add(R[0][0]));
 
-		for (int k1 = -N1 / 2; k1 < N1 / 2; k1++) {
-			for (int k2 = -N2 / 2; k2 < N2 / 2; k2++) {
-				double theta1 = 2 * Math.PI * k1 * n1 / N1;
-				double theta2 = 2 * Math.PI * k2 * n2 / N2;
-				Complex W1 = new Complex(Math.cos(theta1), Math.sin(theta1));
-				Complex W2 = new Complex(Math.cos(theta2), Math.sin(theta2));
-				r = r.add(W1.multiply(W2).multiply(R[k1 + N1 / 2][k2 + N2 / 2]));
+		for (int k1 = 0; k1 < N1 / 2; k1++) {
+			for (int k2 = 0; k2 < N2 / 2; k2++) {
+
+				W1 = this.W1.get(Integer.valueOf(k1 * n1));
+				W2 = this.W2.get(Integer.valueOf(k2 * n2));
+
+				// k1>0 && k2>0
+				Complex a = W1.multiply(W2).multiply(
+						R[k1 + N1 / 2][k2 + N2 / 2]);
+				// k1>0 && k2<0
+				Complex b = W1.multiply(W2.conjugate()).multiply(
+						R[k1 + N1 / 2][-k2 + N2 / 2]);
+				// k1<0 && k2>0
+				Complex c = W1.conjugate().multiply(W2)
+						.multiply(R[-k1 + N1 / 2][k2 + N2 / 2]);
+				// k1<0 && k2<0
+				Complex d = W1.multiply(W2).conjugate()
+						.multiply(R[-k1 + N1 / 2][-k2 + N2 / 2]);
+				r = (k1 == 0 && k2 == 0) ? r : r.add(a).add(b).add(c).add(d);
 
 			}
 		}
 
-		return r.divide(new Complex(N1*N2,0));
-		
+		return r.divide(new Complex(N1 * N2, 0));
+
 	}
 
 	/**
@@ -119,7 +178,7 @@ public class DFTMatcher extends GenericMatcher implements IMatcher {
 			for (int n2 = -N2 / 2; n2 < N2 / 2; n2++) {
 				r[n1 + N1 / 2][n2 + N2 / 2] = getIDFT(R, n1, n2);
 			}
-		}		
+		}
 		return r;
 	}
 
