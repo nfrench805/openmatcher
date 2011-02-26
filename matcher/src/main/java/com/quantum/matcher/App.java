5fc408f19641c8b54283ee5a1c17da757396ddb3
pascaldergane <pascaldergane@gmail.com>
2011-02-26 15:00:12 +0000

    adding new unit test for rotation

--- matcher/src/main/java/com/quantum/matcher/App.java
+++ matcher/src/main/java/com/quantum/matcher/App.java
@@ -1,11 +1,30 @@
 package com.quantum.matcher;
 
 /**
- * Hello world!
+ * Matching Unit
  * 
  */
 public class App {
+	
+	private String refFilename;
+	private String candFilename;
+	private String algorithm;
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
+		
+
+	}
+
+	public static void usage() {
+		System.out.println("Usage:");
+		System.out.println("-h to get this help");
+		System.out
+				.println("-ref <filename_path> to define a filename image as reference");
+		System.out
+				.println("-cand <filename_path> to define a filename image as candidate");
+		System.out
+				.println("-alg <DFT|FFT> to define algorithm to use; by default FFT");
+
 	}
 }
