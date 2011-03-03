1b6253e790b666cf96a25f44d170b3f4a9af7f49
pascaldergane <pascaldergane@gmail.com>
2011-03-03 23:00:18 +0000

    adding multidimensional methods

--- matcher/src/main/java/com/quantum/algorithms/fourier/FourierTransform.java
+++ matcher/src/main/java/com/quantum/algorithms/fourier/FourierTransform.java
@@ -71,14 +71,15 @@ public class FourierTransform {
 		int N = input.length;
 		Complex projection_j = new Complex(0, 0);
 		double theta = 2 * Math.PI * j / N;
-			
-		for (int k = 0; k < N; k++) {			
-			//double theta2= 2 * Math.PI * (N-k-1) * j / N;
-			//exp(-i*theta2) = exp (+ i *2 * pi * (k+1) * j /N) = W_kj.conjugate * exp (i*2*pi*j/N)			
-			Complex w_jk = new Complex(Math.cos(theta*k),
-					- Math.sin(theta*k)) ;
-			if (!forward){
-				w_jk=w_jk.conjugate();
+
+		for (int k = 0; k < N; k++) {
+			// double theta2= 2 * Math.PI * (N-k-1) * j / N;
+			// exp(-i*theta2) = exp (+ i *2 * pi * (k+1) * j /N) =
+			// W_kj.conjugate * exp (i*2*pi*j/N)
+			Complex w_jk = new Complex(Math.cos(theta * k),
+					-Math.sin(theta * k));
+			if (!forward) {
+				w_jk = w_jk.conjugate();
 			}
 			projection_j = projection_j.add(w_jk.multiply(input[k]));
 		}
@@ -88,7 +89,7 @@ public class FourierTransform {
 			return (projection_j.divide(new Complex(N, 0)));
 		}
 	}
-	
+
 	/**
 	 * return transformed Matrix (dft if forward is true, inverse dft else)
 	 * multiDimensionalComplexMatrix input
@@ -97,17 +98,70 @@ public class FourierTransform {
 	 * @param forward
 	 * @return
 	 */
-	public static Object transformMultiDimensionalComplexMatrix(Object input,boolean forward){
-		Object output = null;
-		//mdcm =        Complex[10][20]...[100]
-		MultiDimensionalComplexMatrix mdcm = new MultiDimensionalComplexMatrix(input);
-		//dimensionSize=Integer[10,20,....,100]
-		Integer[] dimensionSize = mdcm.getDimensionSizes();
-				
-        //cycle through each dimension
-        for (int i = 0; i < dimensionSize.length; i++) {
-            
-        }               
-		return output;
+	public static Object transformMultiDimensionalComplexMatrix(Object input,
+			boolean forward) {
+		// mdcm = Complex[10][20]...[100]
+		MultiDimensionalComplexMatrix output = (MultiDimensionalComplexMatrix) (new MultiDimensionalComplexMatrix(
+				input));
+		// dimensionSize=Integer[10,20,....,100]
+		Integer[] dimensionSize = (Integer[]) output.getDimensionSizes();
+
+		// cycle through each dimension
+		for (int d = 0; d < dimensionSize.length; d++) {
+			output = transform(output, forward, d, new int[0]);
+		}
+		return output.getArray();
+	}
+
+	/**
+	 * compute DFT along one dimension
+	 * 
+	 * @param input
+	 * @param forward
+	 * @param dimension
+	 * @param subVector
+	 * @return
+	 */
+	public static MultiDimensionalComplexMatrix transform(
+			MultiDimensionalComplexMatrix input, final boolean forward,
+			final int dimension, int[] subVector) {
+
+		Integer[] dimensionSize = (Integer[]) input.getDimensionSizes();
+
+		if (subVector.length == dimensionSize.length) {
+			// temp will store intermediate DFT (or IDFT) 1-dimension
+			// along dimension 'dimension'
+			// firstly, initialize temp with data from input
+			Complex[] temp = new Complex[dimensionSize[dimension]];
+			for (int i = 0; i < dimensionSize[dimension]; i++) {
+				subVector[dimension] = i;
+				temp[i] = input.getElement(subVector);
+			}
+			// secondly, call transform (1-dimension)
+			temp = transform(temp, forward);
+
+			// finally, recopy result into multi dimensional array
+			for (int i = 0; i < dimensionSize[dimension]; i++) {
+				subVector[dimension] = i;
+				input.set(temp[i], subVector);
+			}
+		} else {
+			int[] vector = new int[subVector.length + 1];
+			System.arraycopy(subVector, 0, vector, 0, subVector.length);
+			if (subVector.length == dimension) {
+				// value is not important once the recursion is done.
+				// then an dft will be applied along the dimension d.
+				vector[dimension] = 0;
+				input = transform(input, forward, dimension, vector);
+			} else {
+				for (int i = 0; i < dimensionSize[subVector.length]; i++) {
+					vector[subVector.length] = i;
+					// further split along the next dimension
+					input = transform(input, forward, dimension, vector);
+				}
+			}
+		}
+		// and return input transformed along dimension
+		return input;
 	}
 }
