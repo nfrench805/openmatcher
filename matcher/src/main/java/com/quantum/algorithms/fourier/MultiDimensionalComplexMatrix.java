782d512c4eee5d8cc79c576c4a60ceed10b4d207
pascaldergane@gmail.com <pascaldergane@gmail.com@6a39bacd-c753-ab57-8c4b-ff1082b993e4>
2011-03-03 23:00:18 +0000

    adding multidimensional methods

--- matcher/src/main/java/com/quantum/algorithms/fourier/MultiDimensionalComplexMatrix.java
+++ matcher/src/main/java/com/quantum/algorithms/fourier/MultiDimensionalComplexMatrix.java
@@ -3,6 +3,7 @@
  */
 package com.quantum.algorithms.fourier;
 
+import java.lang.reflect.Array;
 import java.util.Vector;
 
 import org.apache.commons.math.MathRuntimeException;
@@ -12,12 +13,11 @@ import org.apache.commons.math.complex.Complex;
  * @author Pascal Dergane
  * 
  */
-public class MultiDimensionalComplexMatrix {
+public class MultiDimensionalComplexMatrix implements Cloneable {
 
-	 /** Message for dimension mismatch. */
-    private static final String DIMENSION_MISMATCH_MESSAGE =
-        "some dimensions don't match: {0} != {1}";
-    
+	/** Message for dimension mismatch. */
+	private static final String DIMENSION_MISMATCH_MESSAGE = "some dimensions don't match: {0} != {1}";
+
 	/**
 	 * array of multiDimensions
 	 */
@@ -35,52 +35,138 @@ public class MultiDimensionalComplexMatrix {
 		for (Object lastDimension = multiDimensionalMatrixArray; lastDimension instanceof Object[];) {
 			final Object[] array = (Object[]) lastDimension;
 			numOfDimensions++;
-			dimensionSize.add(numOfDimensions, array.length);
+			dimensionSize.add(array.length);
 			lastDimension = array[0];
 		}
 	}
-	
+
 	/**
-     * Get the size in all dimensions.
-     * @return size in all dimensions
-     */
-    public Integer[] getDimensionSizes() {    	
-        return (Integer[]) dimensionSize.toArray();
-    }
+	 * Get the size in all dimensions.
+	 * 
+	 * @return size in all dimensions
+	 */
+	public Integer[] getDimensionSizes() {
+		Object[] outputObject = dimensionSize.toArray();
+		Integer[] output = new Integer[outputObject.length];
+		for (int i=0;i<outputObject.length;i++){
+			output[i] = (Integer) outputObject[i];			
+		}
+		return output;
+	}
 
-    /**
-     * Get the underlying storage array
-     * @return underlying storage array
-     */
-    public Object getArray() {
-        return multiDimensionalMatrixArray;
-    }
-    
-    /**
-     * return element (coordinates represented by Vector)
-     * of multiDimensionalMatrixArray
-     * 
-     * for example
-     * multiDimensionalMatrixArray = Complex[][]
-     * getElement(1,3) return Complex[1][3]
-     * @param Vector
-     * @return Complex value 
-     */
-    public Complex getElement(int... coordinates){
-    	if (null == coordinates){
-    		return null;
-    	}
-    	if (coordinates.length != dimensionSize.size()) {
-            throw MathRuntimeException.createIllegalArgumentException(
-                    DIMENSION_MISMATCH_MESSAGE, coordinates.length, dimensionSize.size());
-        }
-    	
-    	Object lastDimension = multiDimensionalMatrixArray;
+	/**
+	 * Get the underlying storage array
+	 * 
+	 * @return underlying storage array
+	 */
+	public Object getArray() {
+		return multiDimensionalMatrixArray;
+	}
 
-        for (int i = 0; i < dimensionSize.size(); i++) {
-            lastDimension = ((Object[]) lastDimension)[coordinates[i]];
-        }
-        
-        return (Complex) lastDimension;
-    }
+	/**
+	 * return element (coordinates represented by Vector) of
+	 * multiDimensionalMatrixArray
+	 * 
+	 * for example multiDimensionalMatrixArray = Complex[][] getElement(1,3)
+	 * return Complex[1][3]
+	 * 
+	 * @param Vector
+	 * @return Complex value
+	 */
+	public Complex getElement(int... coordinates) {
+		if (null == coordinates) {
+			return null;
+		}
+		if (coordinates.length != dimensionSize.size()) {
+			throw MathRuntimeException.createIllegalArgumentException(
+					DIMENSION_MISMATCH_MESSAGE, coordinates.length,
+					dimensionSize.size());
+		}
+
+		Object lastDimension = multiDimensionalMatrixArray;
+
+		for (int i = 0; i < dimensionSize.size(); i++) {
+			lastDimension = ((Object[]) lastDimension)[coordinates[i]];
+		}
+
+		return (Complex) lastDimension;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public Object clone() {
+		MultiDimensionalComplexMatrix mdcm = new MultiDimensionalComplexMatrix(
+				Array.newInstance(Complex.class, this.getDimensionSizes().length));
+		clone(mdcm);
+		return mdcm;
+	}
+
+	/**
+	 * Copy contents of current array into mdcm.
+	 * 
+	 * @param mdcm
+	 *            array where to copy data
+	 */
+	private void clone(MultiDimensionalComplexMatrix mdcm) {
+		int dimensionSizes = dimensionSize.size();
+		int[] vector = new int[dimensionSizes];
+		int size = 1;
+		for (int i = 0; i < dimensionSizes; i++) {
+			size *= dimensionSize.elementAt(i);
+		}
+		int[][] vectorList = new int[size][dimensionSizes];
+		for (int[] nextVector : vectorList) {
+			System.arraycopy(vector, 0, nextVector, 0, dimensionSizes);
+			for (int i = 0; i < dimensionSizes; i++) {
+				vector[i]++;
+				if (vector[i] < dimensionSize.elementAt(i)) {
+					break;
+				} else {
+					vector[i] = 0;
+				}
+			}
+		}
+
+		for (int[] nextVector : vectorList) {
+			mdcm.set(getElement(nextVector), nextVector);
+		}
+	}
+
+	/**
+	 * Set a matrix element.
+	 * 
+	 * @param magnitude
+	 *            magnitude of the element
+	 * @param vector
+	 *            indices of the element
+	 * @return the previous value
+	 * @exception IllegalArgumentException
+	 *                if dimensions do not match
+	 */
+	public Complex set(Complex magnitude, int... vector)
+			throws IllegalArgumentException {
+		int dimensionSizes = this.getDimensionSizes().length;
+		if (vector == null) {
+			if (dimensionSizes > 0) {
+				throw MathRuntimeException.createIllegalArgumentException(
+						DIMENSION_MISMATCH_MESSAGE, 0, dimensionSizes);
+			}
+			return null;
+		}
+		if (vector.length != dimensionSizes) {
+			throw MathRuntimeException.createIllegalArgumentException(
+					DIMENSION_MISMATCH_MESSAGE, vector.length, dimensionSizes);
+		}
+
+		Object[] lastDimension = (Object[]) multiDimensionalMatrixArray;
+		for (int i = 0; i < dimensionSizes - 1; i++) {
+			lastDimension = (Object[]) lastDimension[vector[i]];
+		}
+
+		Complex lastValue = (Complex) lastDimension[vector[dimensionSizes - 1]];
+		lastDimension[vector[dimensionSizes - 1]] = magnitude;
+
+		return lastValue;
+	}
+
 }
--- matcher/src/test/java/com/quantum/algorithms/fourier/FourierTransformTest.java
+++ matcher/src/test/java/com/quantum/algorithms/fourier/FourierTransformTest.java
@@ -14,7 +14,7 @@ import org.junit.Test;
  * 
  */
 public class FourierTransformTest extends TestCase {
-	private int size = 1024;
+	private int size = 128;
 	/**
 	 * define a complex serie
 	 */
@@ -24,16 +24,26 @@ public class FourierTransformTest extends TestCase {
 	 */
 	private Complex[] r;
 
+	private Complex[][] multiDimensionalInput;
+
 	@Before
 	public void setUp() throws Exception {
 		f = new Complex[size];
 		r = new Complex[size];
+		multiDimensionalInput = new Complex[size][size];
 
 		for (int i = 0; i < size; i++) {
 			f[i] = new Complex(Math.round(Math.random() * 100), Math.round(Math
 					.random() * 100));
 
 			r[i] = new Complex(Math.round(Math.random() * 100), 0);
+		}
+
+		for (int x = 0; x < size; x++) {
+			for (int y = 0; y < size; y++) {
+				multiDimensionalInput[x][y] = new Complex(Math.round(Math
+						.random() * 100), Math.round(Math.random() * 100));
+			}
 		}
 
 	}
@@ -90,11 +100,11 @@ public class FourierTransformTest extends TestCase {
 			final Complex X = dft[k];
 			expected += x.abs() * x.abs();
 			phase += X.abs() * X.abs();
-		}		
-		//System.out.println("expected=" + expected + " phase=" + phase/size);
-		assertEquals(Math.round(expected),Math.round(phase/size));
+		}
+		// System.out.println("expected=" + expected + " phase=" + phase/size);
+		assertEquals(Math.round(expected), Math.round(phase / size));
 	}
-	
+
 	/**
 	 * check Plancherel theorem sum (x*x.conjugate) = 1/N * sum (X*X.conjugate)
 	 */
@@ -102,8 +112,8 @@ public class FourierTransformTest extends TestCase {
 		Complex[] dft = FourierTransform.dft(f);
 		Complex[] dft2 = FourierTransform.dft(r);
 
-		Complex expected = new Complex(0,0);
-		Complex phase = new Complex(0,0);
+		Complex expected = new Complex(0, 0);
+		Complex phase = new Complex(0, 0);
 		for (int k = 0; k < size; k++) {
 			final Complex x = f[k];
 			final Complex y = r[k];
@@ -111,11 +121,33 @@ public class FourierTransformTest extends TestCase {
 			final Complex Y = dft2[k];
 			expected = expected.add(x.multiply(y.conjugate()));
 			phase = phase.add(X.multiply(Y.conjugate()));
-		}		
-		//System.out.println("expected=" + expected + " phase=" + phase);
-		assertEquals(Math.round(expected.getReal()),Math.round(phase.getReal()/size));
-		assertEquals(Math.round(expected.getImaginary()),Math.round(phase.getImaginary()/size));
+		}
+		// System.out.println("expected=" + expected + " phase=" + phase);
+		assertEquals(Math.round(expected.getReal()),
+				Math.round(phase.getReal() / size));
+		assertEquals(Math.round(expected.getImaginary()),
+				Math.round(phase.getImaginary() / size));
 	}
 
+	/**
+	 * test DFT and IDFT of multi Dimensional input check reversibility
+	 */
+	public void testDFTMultiDimensional() {
+		Complex[][] mdft =(Complex[][]) FourierTransform
+				.transformMultiDimensionalComplexMatrix(multiDimensionalInput,
+						true);
+		Complex[][] imdft = (Complex[][])FourierTransform
+				.transformMultiDimensionalComplexMatrix(mdft, false);
+
+		for (int x = 0; x < size; x++) {
+			for (int y = 0; y < size; y++) {
+				assertEquals(Math.round(Math.round(multiDimensionalInput[x][y]
+						.getReal())), Math.round(((Complex[][]) imdft)[x][y].getReal()));
+				assertEquals(
+						Math.round(multiDimensionalInput[x][y].getImaginary()),
+						Math.round(((Complex[][]) imdft)[x][y].getImaginary()));
+			}
+		}
+	}
 
 }
