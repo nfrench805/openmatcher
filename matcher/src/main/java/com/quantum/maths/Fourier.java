7d31b8b7909e703594eecfa7b4a227c51f19fa28
pascaldergane <pascaldergane@gmail.com>
2011-03-15 23:46:20 +0000

    adding fourier method 2-radix (4-radix not complete) instable version

--- matcher/src/main/java/com/quantum/maths/Fourier.java
+++ matcher/src/main/java/com/quantum/maths/Fourier.java
@@ -10,6 +10,11 @@ import org.apache.commons.math.complex.Complex;
  * 
  */
 public class Fourier {
+
+	private static Complex norm2 = new Complex(Math.sqrt(2), 0);
+
+	// private static Complex norm4 = new Complex(Math.sqrt(4), 0);
+
 	/**
 	 * compute Discrete Fourier Transform known as DFT (if forward = true) or
 	 * Inverse DFT (if forward = false) of input signal
@@ -24,91 +29,103 @@ public class Fourier {
 	public static Complex[] transform(final Complex[] input, boolean forward) {
 
 		final int N = input.length;
+		Complex[] S = new Complex[N];
+
 		if (N == 1) {
-			return input;
+			S[0] = input[0];
+
+		} else if (N == 2) {
+			// N is even, we can use radix-2
+			S[0] = (input[0].add(input[1])).divide(norm2);
+			S[1] = input[0].subtract(input[1]).divide(norm2);
+			return S;
 		}
 
-		final double omega = 2 * Math.PI / N;
-		final Complex Wn = forward ? new Complex(0, -omega).exp()
-				: new Complex(0, omega).exp();
-		final Complex norm = new Complex(2,0);
-		Complex[] S = new Complex[N];
-		//TODO: complete 4-radix method
-		if (N % 4 == 0) {
-			//we can use radix-4
-			
-			
-			for (int q=0;q<=N/4 -1;q++){
-				for (int p=0;p<4;p++){
-					S[N/4*p+q] = 	
-				}
-			 
-			}
-		}
+		final double omega = -2 * Math.PI / N;
+		final Complex Wn = forward ? new Complex(0, omega).exp() : new Complex(
+				0, -omega).exp();
+
+//		if (N % 4 == 0 && forward) {
+//
+//			Complex[] even = new Complex[N / 2];
+//			Complex[] odd1 = new Complex[N / 4];
+//			Complex[] odd2 = new Complex[N / 4];
+//
+//			odd1[0] = input[1];
+//			odd2[0] = input[N / 4 - 1];
+//
+//			for (int k = 0; k < even.length; k++) {
+//				even[k] = input[2 * k];
+//				if (k < odd1.length & k > 0) {
+//					odd1[k] = input[4 * k + 1];
+//					odd2[k] = input[4 * k - 1];
+//				}
+//			}
+//
+//			even = transform(even, forward);
+//			odd1 = transform(odd1, forward);
+//			odd2 = transform(odd2, forward);
+//
+//			for (int k = 0; k < N / 4; k++) {
+//				final Complex Wnk = Wn.pow(new Complex(k, 0));
+//				final Complex Wnkminus = Wn.conjugate();
+//
+//				Complex f1 = (Wnk.multiply(odd1[k])).add((Wnkminus
+//						.multiply(odd2[k])));
+//				Complex f2 = (Wnk.multiply(odd1[k])).subtract((Wnkminus
+//						.multiply(odd2[k])));
+//				
+//				f2 = Complex.I.negate().multiply(odd1[k]);
+//				
+//				S[k] = (even[k].add(odd1[k].multiply(Wnk))).divide(norm2);
+//				S[k + N / 2] = (even[k].subtract((odd1[k].multiply(Wnk)))).divide(norm2);
+//				
+//				S[k + N / 4] = (even[k + N / 4].add(f2)).divide(norm2);
+//				S[k + 3 * N / 4] = (even[k + N / 4].subtract(f2)).divide(norm2);
+//				
+//
+//				// final Complex w4 = forward ? Complex.I.negate().multiply(
+//				// (Wn.pow(new Complex(k, 0)))) : Complex.I.multiply((Wn
+//				// .pow(new Complex(k, 0))));				
+//				// final Complex f4 = odd[k + N / 4].multiply(w4);
+//				
+//				//final Complex f2 = odd[k].multiply(w);
+//				//S[k] = even[k].add(f2).divide(norm2);
+//				S[k + N / 2] = even[k].subtract(f2).divide(norm2);
+//				// S[k + N / 4] = even[k + N / 4].add(f4).divide(norm2);
+//				
+//				// S[k + 3 * N / 4] = even[k + N /
+//				// 4].subtract(f4).divide(norm2);
+//			}
+//			return S;
+//		}
+
 		if (N % 2 == 0) {
-			// N is even, we can use radix-2
 			Complex[] even = new Complex[N / 2];
 			Complex[] odd = new Complex[N / 2];
 			for (int k = 0; k < input.length / 2; k++) {
 				even[k] = input[2 * k];
 				odd[k] = input[2 * k + 1];
 			}
-
 			even = transform(even, forward);
 			odd = transform(odd, forward);
 
-			for (int k = 0; k < N/2; k++) {
+			for (int k = 0; k < N / 2; k++) {
 				final Complex w = Wn.pow(new Complex(k, 0));
-				final Complex f1 = even[k];
+				// final Complex w4 = forward ? Complex.I.negate().multiply(
+				// (Wn.pow(new Complex(k, 0)))) : Complex.I.multiply((Wn
+				// .pow(new Complex(k, 0))));
 				final Complex f2 = odd[k].multiply(w);
-				
-				S[k] = (forward) ? f1.add(f2) : f1
-						.add(f2).divide(norm);
-				S[k+N/2] = (forward)?f1.add(f2.negate()):f1.add(f2.negate()).divide(norm);
+				// final Complex f4 = odd[k + N / 4].multiply(w4);
+				S[k] = even[k].add(f2).divide(norm2);
+				// S[k + N / 4] = even[k + N / 4].add(f4).divide(norm2);
+				S[k + N / 2] = even[k].subtract(f2).divide(norm2);
+				// S[k + 3 * N / 4] = even[k + N /
+				// 4].subtract(f4).divide(norm2);
 			}
 		}
+
 		return S;
-	}
-
-	/**
-	 * return kth item of input fourier transform
-	 * 
-	 * @param input
-	 *            signal to transform
-	 * @param forward
-	 *            if true, compute DFT, else compute IDFT
-	 * @param k
-	 *            position of item to compute
-	 * @return
-	 */
-	private static Complex transform(Complex[] input, boolean forward, int k) {
-		int N = input.length;
-		Complex S = new Complex(0, 0);
-		double omega = (forward) ? -2 * Math.PI * k / N : 2 * Math.PI * k / N;
-		Complex Wn = new Complex(0, -2 * Math.PI / N).exp();
-		Complex Wkn = Wn.pow(new Complex(k, 0));
-
-		if (N % 2 == 0) {
-			Complex[] even = new Complex[N / 2];
-			Complex[] odd = new Complex[N / 2];
-
-			for (int n = 0; n < N / 2; n++) {
-				even[n] = input[2 * n];
-				odd[n] = input[2 * n + 1];
-			}
-
-			S = (forward) ? transform(even, forward, k).add(
-					transform(odd, forward, k).multiply(Wkn)) : transform(even,
-					forward, k).add(
-					transform(odd, forward, k).multiply(Wkn.conjugate()));
-			return (forward) ? S : S.divide(new Complex(N, 0));
-		} else {
-			for (int n = 0; n < N; n++) {
-				S = S.add(input[n].multiply((new Complex(0, omega * n)).exp()));
-			}
-			return (forward) ? S : S.divide(new Complex(N, 0));
-		}
-
 	}
 
 }
--- matcher/src/test/java/com/quantum/maths/FourierTests.java
+++ matcher/src/test/java/com/quantum/maths/FourierTests.java
@@ -14,20 +14,19 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
-
-
 public class FourierTests extends TestCase {
 
 	private FastFourierTransformer FFT = new FastFourierTransformer();
 	private int N = 8192;
 	private Complex[] input = new Complex[N];
+	private Complex[] inputReal = new Complex[N];
 
 	@Before
 	public void setUp() throws Exception {
 		for (int k = 0; k < N; k++) {
 			input[k] = new Complex(Math.rint(Math.random() * 255),
 					Math.rint(Math.random() * 255));
+			inputReal[k] = new Complex(Math.rint(Math.random() * 255), 0);
 		}
 
 	}
@@ -59,24 +58,52 @@ public class FourierTests extends TestCase {
 		Complex[] DFT = Fourier.transform(input, true);
 		Complex[] IDFT = Fourier.transform(DFT, false);
 		long end = System.currentTimeMillis();
-		long elapsedTime = end-start;
-		System.out.println("elapsed time(ms) ="+elapsedTime);
-		
+		long elapsedTime = end - start;
+		System.out.println("elapsed time(ms) =" + elapsedTime);
+
 		long start1 = System.currentTimeMillis();
 		Complex[] DFT1 = FFT.transform(input);
 		Complex[] IDFT1 = FFT.inversetransform(DFT1);
 		long end1 = System.currentTimeMillis();
-		System.out.println("Apache FFT elapsed time(ms) ="+(end1 - start1));
-		
+		long apacheElapsedTime = (end1 - start1);
+		System.out.println("Apache FFT elapsed time(ms) =" + apacheElapsedTime);
+
 		for (int k = 0; k < N; k++) {
-			
-			//System.out.println(k+" >"+input[k].getReal()+","+input[k].getImaginary());
-			//System.out.println(k+" >"+IDFT[k].getReal()+","+IDFT[k].getImaginary());
-			//System.out.println("------------------------------------------------------");
+
+//			 System.out.println(k+" >"+input[k].getReal()+","+input[k].getImaginary());
+//			 System.out.println(k+" >"+IDFT[k].getReal()+","+IDFT[k].getImaginary());
+//			 System.out.println(k+" >"+IDFT1[k].getReal()+","+IDFT1[k].getImaginary());
+//			 System.out.println("------------------------------------------------------");
 			assertEquals(Math.round(input[k].getReal()),
 					Math.round(IDFT[k].getReal()));
 			assertEquals(Math.round(input[k].getImaginary()),
 					Math.round(IDFT[k].getImaginary()));
+
+		}
+	}
+
+	/**
+	 * check DFT of real signal X[k] = X[N-k].conjugate
+	 */
+	@Test
+	public void testTFDRealSignal() {
+
+		long start = System.currentTimeMillis();
+		Complex[] DFT = Fourier.transform(inputReal, true);
+		//Complex[] IDFT = Fourier.transform(DFT, false);
+		long end = System.currentTimeMillis();
+		long elapsedTime = end - start;
+		System.out.println("elapsed time(ms) =" + elapsedTime);
+		
+		for (int k = 0; k < N/2; k++) {
+
+//			System.out.println(k+" >"+DFT[k].getReal()+","+DFT[k].getImaginary());
+//			System.out.println(k+" >"+DFT[(N-k) % N].getReal()+","+DFT[(N-k)%N].getImaginary());
+//			System.out.println("------------------------------------------------------");
+			assertEquals(Math.round(DFT[k].getReal()),
+					Math.round(DFT[(N-k)%N].conjugate().getReal()));
+			assertEquals(Math.round(DFT[k].getImaginary()),
+					Math.round(DFT[(N-k)%N].conjugate().getImaginary()));
 
 		}
 	}
