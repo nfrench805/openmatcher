8cb8ce7d027cc20517c8dab731d1a9bb8435c906
pascaldergane <pascaldergane@gmail.com>
2011-03-22 10:04:18 +0000

    optimize fourier Radix 2 and stabilize radix4 finally implementation of Fourier is cancelled. All performance shows that radix-4 Apache implementation is best one

--- matcher/src/main/java/com/quantum/maths/Fourier.java
+++ matcher/src/main/java/com/quantum/maths/Fourier.java
@@ -4,6 +4,7 @@
 package com.quantum.maths;
 
 import org.apache.commons.math.complex.Complex;
+import org.apache.commons.math.transform.FastFourierTransformer;
 
 /**
  * @author Pascal Dergane
@@ -13,6 +14,8 @@ public class Fourier {
 
 	private static Complex norm2 = new Complex(2, 0);
 	private static Complex[][] root;
+	private static double _2PI = Math.PI * 2;
+	private static FastFourierTransformer FFT = new FastFourierTransformer();
 
 	/**
 	 * compute Discrete Fourier Transform known as DFT (if forward = true) or
@@ -26,10 +29,9 @@ public class Fourier {
 	 * @return transformed signal
 	 */
 	public static Complex[] transform(final Complex[] input, boolean forward) {
-		//root = initializeRoot(4, forward);
-		//return forward?transformRadix4(input):inverseTransformRadix4(input);
-		 return forward ? transformRadix2(input) :
-		 inverseTransformRadix2(input);
+		// root = initializeRoot(4, forward);
+		// return forward?transformRadix4(input):inverseTransformRadix4(input);
+		return forward ? transformRadix2(input) : inverseTransformRadix2(input);
 	}
 
 	/**
@@ -146,7 +148,7 @@ public class Fourier {
 			root[0][1] = root[0][0];
 			root[1][0] = root[0][0];
 			root[1][1] = new Complex(-1, 0);
-			break;		
+			break;
 		default:
 			for (int p = 0; p < radixLength; p++) {
 				for (int l = 0; l < radixLength; l++) {
@@ -173,61 +175,55 @@ public class Fourier {
 	 *            according with FFT-radix method
 	 * @return transformed signal
 	 */
-	public static Complex[] transformRadixP(final Complex[] x,
-			final boolean forward, final int p) {
+	public static Complex[] transformSplitRadix(final Complex[] x) {
 
-		final int radixLength = p;
-		final int N = x.length;
-		Complex[] S = new Complex[N];
+		Complex[] X = new Complex[x.length];
 
-		switch (N) {
+		switch (x.length) {
 		case 1:
-			S[0] = x[0];
+			X[0] = x[0];
 			break;
 		case 2:
-			S[0] = forward ? (x[0].add(x[1])) : (x[0].add(x[1])).divide(norm2);
-			S[1] = forward ? x[0].subtract(x[1]) : x[0].subtract(x[1]).divide(
-					norm2);
+			X[0] = x[0].add(x[1]);
+			X[1] = x[0].subtract(x[1]);
 			break;
 		default:
-			final int M = N / radixLength;
-			Complex[][] f = new Complex[radixLength][M];
-			// Complex[] radix = new Complex[radixLength];
+			Complex[] U = new Complex[x.length / 2];
+			Complex[] Z = new Complex[x.length / 4];
+			Complex[] Y = new Complex[x.length / 4];
 
-			Complex[][] root = initializeRoot(radixLength, forward);
-
-			final double omega = forward ? -2 * Math.PI / N : 2 * Math.PI / N;
-
-			// initialize f by splitting x in radixLength
-			for (int i = 0; i < radixLength; i++) {
-				for (int k = 0; k < M; k++) {
-					f[i][k] = x[radixLength * k + i];
+			for (int k = 0; k < x.length / 2; k++) {
+				U[k] = x[2 * k];
+				if (k < x.length / 4) {
+					Z[k] = x[4 * k + 1];
+					Y[k] = x[4 * k + 3];
 				}
-				f[i] = transformRadixP(f[i], forward, p);
+			}			
+			U = transformRadix2(U);
+//			Z = transformSplitRadix(Z);
+//			Y = transformSplitRadix(Y);
+			Z = FFT.transform(Z);
+			Y = FFT.transform(Y);
+
+			Complex Wn = new Complex(0, -_2PI / x.length);
+
+			for (int k = 0; k < x.length / 4; k++) {
+				Complex Wnk = Wn.multiply(k).exp();
+				Complex Wn3k = Wn.multiply(3 * k).exp();
+
+				Complex a = Z[k].multiply(Wnk);
+				Complex b = Y[k].multiply(Wn3k);
+				Complex c = Complex.I.multiply(a.subtract(b));
+
+				X[k] = U[k].add(a.add(b));
+				X[k + x.length / 2] = U[k].subtract(a.add(b));
+				X[k + x.length / 4] = U[k + x.length / 4].subtract(c);
+				X[k + 3 * x.length / 4] = U[k + x.length / 4].add(c);
 			}
 
-			Complex Wn = new Complex(0, omega);
-
-			for (int n = 0; n < M; n++) {
-				for (int l = 0; l < radixLength; l++) {
-					final int index = n + l * M;
-					S[index] = f[0][n];
-					for (int i = 1; i < radixLength; i++) {
-						Complex Wpn = (Wn.multiply(i * n)).exp();
-						// root[p][l] = forward ? (new Complex(0, -2 * Math.PI *
-						// p * l
-						// / radixLength)).exp() : (new Complex(0, 2 * Math.PI
-						// * p * l / radixLength)).exp();
-						S[index] = S[index].add(f[i][n].multiply(root[l][i]
-								.multiply(Wpn)));
-					}
-					S[index] = forward ? S[index] : S[index]
-							.multiply((1.0 / radixLength));
-				}
-			}
-			break;
 		}
-		return S;
+		return X;
+
 	}
 
 	/**
@@ -277,21 +273,13 @@ public class Fourier {
 			Complex Wn = new Complex(0, omega);
 
 			for (int n = 0; n < M; n++) {
-				S[n] = f[0][n];
-				S[n + M] = f[0][n];
-				S[n + 2 * M] = f[0][n];
-				S[n + 3 * M] = f[0][n];
-
-				for (int i = 1; i < radixLength; i++) {
-					Complex Wpn = (Wn.multiply(i * n)).exp();
-					S[n] = S[n].add(f[i][n].multiply(Wpn));
-					S[n + M] = S[n + M].add(f[i][n].multiply(root[1][i]
-							.multiply(Wpn)));
-					S[n + 2 * M] = S[n + 2 * M].add(f[i][n].multiply(root[2][i]
-							.multiply(Wpn)));
-					S[n + 3 * M] = S[n + 3 * M].add(f[i][n].multiply(root[3][i]
-							.multiply(Wpn)));
-				}
+				Complex a = Complex.I.multiply(f[1][n]);
+				Complex b = Complex.I.multiply(f[3][n]);
+				S[n] = f[0][n].add(f[1][n]).add(f[2][n]).add(f[3][n]);
+				S[n + M] = f[0][n].subtract(a).subtract(f[2][n]).add(b);
+				S[n + 2 * M] = f[0][n].subtract(f[1][n]).add(f[2][n])
+						.subtract(f[3][n]);
+				S[n + 3 * M] = f[0][n].add(a).subtract(f[2][n]).subtract(b);
 			}
 			break;
 		}
--- matcher/src/test/java/com/quantum/maths/FourierTests.java
+++ matcher/src/test/java/com/quantum/maths/FourierTests.java
@@ -17,15 +17,15 @@ import org.junit.Test;
 public class FourierTests extends TestCase {
 
 	private FastFourierTransformer FFT = new FastFourierTransformer();
-	private int N = 16384/2;
+	private int N = 16384 / 2;
 	private Complex[] input = new Complex[N];
 	private Complex[] inputReal = new Complex[N];
 
 	@Before
 	public void setUp() throws Exception {
 		for (int k = 0; k < N; k++) {
-			input[k] = new Complex(Math.rint(Math.random() * 255),
-					Math.rint(Math.random() * 255));
+			// input[k] = new Complex(Math.rint(Math.random() * 255),
+			// Math.rint(Math.random() * 255));
 			inputReal[k] = new Complex(Math.rint(Math.random() * 255), 0);
 		}
 
@@ -53,6 +53,58 @@ public class FourierTests extends TestCase {
 	}
 
 	@Test
+	public void testDFT() {
+		long apacheElapsedTime = 0;
+		long elapsedTime = 0;
+		long elapsedTime2 = 0;
+
+		final int attempts = 200;
+		for (int j = 0; j < attempts; j++) {
+			for (int k = 0; k < N; k++) {
+				input[k] = new Complex(Math.rint(Math.random() * 255),
+						Math.rint(Math.random() * 255));
+			}
+
+			long start1 = System.currentTimeMillis();
+			Complex[] DFT1 = FFT.transform(input);
+			long end1 = System.currentTimeMillis();
+			apacheElapsedTime += (end1 - start1);
+			// System.out.println("Apache FFT elapsed time(ms) ="
+			// + apacheElapsedTime);
+
+			long start = System.currentTimeMillis();
+			Complex[] DFT = Fourier.transformSplitRadix(input);
+			long end = System.currentTimeMillis();
+			elapsedTime += end - start;
+			//System.out.println("Split Radix elapsed time(ms) =" + elapsedTime);
+
+			long start2 = System.currentTimeMillis();
+			Complex[] DFT2 = Fourier.transformRadix2(input);
+			long end2 = System.currentTimeMillis();
+			elapsedTime2 += end2 - start2;
+			//System.out.println("Radix2 elapsed time(ms) =" + elapsedTime2);
+		}
+
+		System.out.println("Average Elapsed Time Apache algorithm:"
+				+ apacheElapsedTime/attempts);
+		System.out.println("Average Elapsed Time Split Radix algorithm:"
+				+ elapsedTime/attempts);
+		System.out.println("Average Elapsed Time Radix-2 algorithm:"
+				+ elapsedTime2/attempts);
+
+		// for (int k = 0; k < N; k++) {
+
+		// System.out.println(k+" >"+input[k].getReal()+","+input[k].getImaginary());
+		// System.out.println(k + " >" + DFT1[k].getReal() + ","
+		// + DFT1[k].getImaginary());
+		// System.out.println(k + " >" + DFT[k].getReal() + ","
+		// + DFT[k].getImaginary());
+		// System.out
+		// .println("------------------------------------------------------");
+		// }
+	}
+
+	@Test
 	public void testTransform() {
 		long start1 = System.currentTimeMillis();
 		Complex[] DFT1 = FFT.transform(input);
@@ -60,26 +112,30 @@ public class FourierTests extends TestCase {
 		long end1 = System.currentTimeMillis();
 		long apacheElapsedTime = (end1 - start1);
 		System.out.println("Apache FFT elapsed time(ms) =" + apacheElapsedTime);
-		
+
 		long start = System.currentTimeMillis();
 		Complex[] DFT = Fourier.transform(input, true);
 		Complex[] IDFT = Fourier.transform(DFT, false);
 		long end = System.currentTimeMillis();
 		long elapsedTime = end - start;
 		System.out.println("Radix 2 elapsed time(ms) =" + elapsedTime);
-	
+
 		for (int k = 0; k < N; k++) {
-			
-//			 System.out.println(k+" >"+input[k].getReal()+","+input[k].getImaginary());
-//			 System.out.println(k+" >"+IDFT1[k].getReal()+","+IDFT1[k].getImaginary());
-//			 System.out.println(k+" >"+IDFT[k].getReal()+","+IDFT[k].getImaginary());
-//			 System.out.println(k+" >"+IDFT2[k].getReal()+","+IDFT2[k].getImaginary());
-//			 System.out.println(k+" >"+IDFT4[k].getReal()+","+IDFT4[k].getImaginary());
-//			 System.out.println(k+" >"+DFT1[k].getReal()+","+DFT1[k].getImaginary());
-//			 System.out.println(k+" >"+DFT[k].getReal()+","+DFT[k].getImaginary());
-//			 System.out.println(k+" >"+DFT2[k].getReal()+","+DFT2[k].getImaginary());
-//			 System.out.println(k+" >"+DFT4[k].getReal()+","+DFT4[k].getImaginary());
-//			 System.out.println("------------------------------------------------------");
+
+			// System.out.println(k+" >"+input[k].getReal()+","+input[k].getImaginary());
+			System.out.println(k + " >" + IDFT1[k].getReal() + ","
+					+ IDFT1[k].getImaginary());
+			System.out.println(k + " >" + IDFT[k].getReal() + ","
+					+ IDFT[k].getImaginary());
+			// System.out.println(k+" >"+IDFT2[k].getReal()+","+IDFT2[k].getImaginary());
+			// System.out.println(k+" >"+IDFT4[k].getReal()+","+IDFT4[k].getImaginary());
+			System.out.println(k + " >" + DFT1[k].getReal() + ","
+					+ DFT1[k].getImaginary());
+			System.out.println(k + " >" + DFT[k].getReal() + ","
+					+ DFT[k].getImaginary());
+			// System.out.println(k+" >"+DFT2[k].getReal()+","+DFT2[k].getImaginary());
+			// System.out.println(k+" >"+DFT4[k].getReal()+","+DFT4[k].getImaginary());
+			// System.out.println("------------------------------------------------------");
 			assertEquals(Math.round(input[k].getReal()),
 					Math.round(IDFT[k].getReal()));
 			assertEquals(Math.round(input[k].getImaginary()),
@@ -95,20 +151,21 @@ public class FourierTests extends TestCase {
 
 		long start = System.currentTimeMillis();
 		Complex[] DFT = Fourier.transform(inputReal, true);
-		//Complex[] IDFT = Fourier.transform(DFT, false);
+		// Complex[] IDFT = Fourier.transform(DFT, false);
 		long end = System.currentTimeMillis();
 		long elapsedTime = end - start;
 		System.out.println("elapsed time(ms) =" + elapsedTime);
-		
-		for (int k = 0; k < N/2; k++) {
 
-//			System.out.println(k+" >"+DFT[k].getReal()+","+DFT[k].getImaginary());
-//			System.out.println(k+" >"+DFT[(N-k) % N].getReal()+","+DFT[(N-k)%N].getImaginary());
-//			System.out.println("------------------------------------------------------");
+		for (int k = 0; k < N / 2; k++) {
+
+			// System.out.println(k+" >"+DFT[k].getReal()+","+DFT[k].getImaginary());
+			// System.out.println(k+" >"+DFT[(N-k) %
+			// N].getReal()+","+DFT[(N-k)%N].getImaginary());
+			// System.out.println("------------------------------------------------------");
 			assertEquals(Math.round(DFT[k].getReal()),
-					Math.round(DFT[(N-k)%N].conjugate().getReal()));
+					Math.round(DFT[(N - k) % N].conjugate().getReal()));
 			assertEquals(Math.round(DFT[k].getImaginary()),
-					Math.round(DFT[(N-k)%N].conjugate().getImaginary()));
+					Math.round(DFT[(N - k) % N].conjugate().getImaginary()));
 
 		}
 	}
